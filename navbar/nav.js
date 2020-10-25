808fd669094f55ab0f14a79b81aa80eaeff22ed8
okorie2 <44427128+okorie2@users.noreply.github.com>
2020-10-25 23:21:30 +0100

    navbar completed, home page in progress

---
+++ navbar/nav.js
@@ -0,0 +1,73 @@
+//dropdown links for "About"
+const navbar= document.getElementById('navbar');
+const navlink= document.getElementById("navbar-links");
+const newlist = document.createElement("li");
+const anchor= document.createElement("a");
+const link = document.createTextNode("gallery");
+addlink= ()=>{
+  anchor.appendChild(link);
+  anchor.title = "gallery";
+  anchor.href = "#";
+  anchor.className= "navbar-link";
+  anchor.style.paddingLeft="2rem";
+  newlist.appendChild(anchor);
+  navlink.appendChild(newlist);
+};
+const link2 = document.createTextNode("partners");
+const newlist2 = document.createElement("li");
+const anchor2= document.createElement("a");
+addlink2=()=>{
+  anchor2.appendChild(link2);
+  anchor2.title="partners";
+  anchor2.href="#";
+  anchor2.className= "navbar-link";
+  anchor2.style.paddingLeft="2rem";
+  newlist2.appendChild(anchor2);
+  newlist.appendChild(newlist2);
+}
+removeLink= ()=>{
+  anchor.classList.remove("navbar-link")
+  newlist.innerHTML=""
+
+
+}
+
+let navlinkclick = navbar.querySelector("#navbar-links");
+navlinkclick.addEventListener("click",()=>{
+  if(anchor.classList.contains("navbar-link")){
+    removeLink();
+  }else {
+    addlink()
+    addlink2()
+  }
+
+});
+//dropdown links for "our work" linnk
+const navlink2= document.getElementById("navbar-links2");
+const worklist = document.createElement("li");
+const workAnchor= document.createElement("a");
+const Worklink = document.createTextNode("project");
+addWorkLink= ()=>{
+  workAnchor.appendChild(link);
+  workAnchor.title = "project";
+  workAnchor.href = "#";
+  workAnchor.className= "navbar-link";
+  workAnchor.style.paddingLeft="2rem";
+  worklist.appendChild(workAnchor);
+  navlink2.appendChild(worklist);
+};
+removeWorkLink= ()=>{
+  workAnchor.classList.remove("navbar-link")
+  worklist.innerHTML=""
+}
+let worklinkclick = navbar.querySelector("#navbar-links2");
+worklinkclick.addEventListener("click",()=>{
+  if(workAnchor.classList.contains("navbar-link")){
+    removeWorkLink();
+  }else {
+    addWorkLink();
+    
+    
+  }
+
+});
--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,59 +1,9 @@
 
-
-const navbar = document.getElementById("navbar");
-const navbarToggle = navbar.querySelector(".navbar-toggle");
-
-function openMobileNavbar() {
-  navbar.classList.add("opened");
-  navbarToggle.setAttribute("aria-label", "Close navigation menu.");
-}
-
-function closeMobileNavbar() {
-  navbar.classList.remove("opened");
-  navbarToggle.setAttribute("aria-label", "Open navigation menu.");
-}
-
-navbarToggle.addEventListener("click", () => {
-  if (navbar.classList.contains("opened")) {
-    closeMobileNavbar();
-  } else {
-    openMobileNavbar();
-  }
-});
-const navbarMenu = navbar.querySelector(".navbar-menu");
-const navbarLinksContainer = navbar.querySelector(".navbar-links");
-
-navbarLinksContainer.addEventListener("click", (clickEvent) => {
-  clickEvent.stopPropagation();
-});
-
-navbarMenu.addEventListener("click", closeMobileNavbar);
-
-const navlink= document.getElementById("navbar-links");
-const newlink = document.createElement("li");
-const anchor= document.createElement("a");
-const link = document.createTextNode("gallery");
-addlink= ()=>{
-  anchor.appendChild(link);
-  anchor.title = "gallery";
-  anchor.href = "#";
-  anchor.className= "navbar-link";
-  newlink.appendChild(anchor);
-  navlink.appendChild(newlink);
-};
-removeLink= ()=>{
-  anchor.classList.remove("navbar-link")
-  newlink.innerHTML=""
-
-
-}
-
-let navlinkclick = navbar.querySelector("#navbar-links");
-navlinkclick.addEventListener("click",()=>{
-  if(anchor.classList.contains("navbar-link")){
-    removeLink();
-  }else{
-    addlink()
-  }
-
+navbar= document.querySelector("navbar")
+jQuery(document).ready(function() {
+	jQuery('.toggle-nav').click(function(e) {
+		jQuery(this).toggleClass('active');
+    jQuery('.menu ul').toggleClass('active');
+    e.preventDefault();
+	});
 });
